0458842f4512dce0108d6068294a06f7ecedb4ee
Philip Ingram <philip.d.ingram@gmail.com>
2014-06-25 15:50:23 -0700

    add simple play stop button

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var app = angular.module('myApp', []);
+
+app.controller('PlayerController', ['$scope', function ($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = 'media/Chewbacca Wookie Noise-SoundBible.com-1201859158.mp3';
+
+  $scope.play = function () {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+
+  $scope.stop = function () {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+
+  $scope.audio.addEventListener('ended', function () {
+    $scope.$apply(function () {
+      $scope.stop();
+    })
+  })
+}])
+
+app.controller('RelatedController', ['$scope', function ($scope) {
+}])
